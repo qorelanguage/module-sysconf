00a7a520dbae36d19c21ba8d8d8c743da98f3637
subik <subik@1b0b35fe-f740-4c1b-b404-a338b32e513b>
2013-08-19 14:55:18 +0000

    sysconf: ported to qpp; new pathconf() function
    git-svn-id: svn+ssh://svn.code.sf.net/p/qore/code/module-sysconf/trunk@6005 1b0b35fe-f740-4c1b-b404-a338b32e513b

--- cmake/FindQore.cmake
+++ cmake/FindQore.cmake
@@ -8,9 +8,37 @@
 # QORE_LIBRARY
 # QORE_MODULES_DIR
 #
-# QORE_EXECUTABLE - qore interpretex binary
+# QORE_EXECUTABLE
+# QORE_QPP_EXECUTABLE
+#
 
 SET(QORE_FOUND FALSE)
+
+
+MACRO (QORE_WRAP_QPP _cpp_files _inputs )
+  message(STATUS "QPP: ${QORE_QPP_EXECUTABLE}")
+  message(STATUS "QPP2: ${_inputs}")
+
+  FOREACH (it ${_inputs})
+    message(STATUS "QPP1: ${it}")
+    GET_FILENAME_COMPONENT(_outfile ${it} NAME_WE)
+    GET_FILENAME_COMPONENT(_infile ${it} ABSOLUTE)
+    SET(_cppfile ${CMAKE_CURRENT_BINARY_DIR}/${_outfile}.cpp)
+    SET(_doxfile ${CMAKE_CURRENT_BINARY_DIR}/${_outfile}.dox.h)
+    message(STATUS "QPP processing: ${_infile} -> ${_cppfile} ${_doxfile}")
+    ADD_CUSTOM_COMMAND(OUTPUT ${_cppfile} ${_doxfile}
+      COMMAND ${QORE_QPP_EXECUTABLE}
+      ARGS --output=${_cppfile} --dox-output=${_doxfile} ${_infile}
+      MAIN_DEPENDENCY ${_infile}
+      WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+      )
+    SET(${_cpp_files} ${${_cpp_files}} ${_cppfile})
+  ENDFOREACH (it)
+
+  message(STATUS "QPP files: ${${_cpp_files}}")
+
+ENDMACRO (QORE_WRAP_QPP)
+
 
 
 FIND_PATH (QORE_INCLUDE_DIR qore/Qore.h
@@ -43,6 +71,11 @@ FIND_PROGRAM(QORE_EXECUTABLE NAMES qore DOC "Searching for qore executable")
 IF (NOT QORE_EXECUTABLE)
     MESSAGE( FATAL_ERROR "Cannot find 'qore' executable. Check if it's in your PATH" )
 ENDIF (NOT QORE_EXECUTABLE)
+
+FIND_PROGRAM(QORE_QPP_EXECUTABLE NAMES qpp DOC "Searching for qore qpp preprocessor")
+IF (NOT QORE_QPP_EXECUTABLE)
+    MESSAGE( FATAL_ERROR "Cannot find 'qpp' executable. Check if it's in your PATH" )
+ENDIF (NOT QORE_QPP_EXECUTABLE)
 
 
 IF (QORE_MIN_VERSION)
--- test/sysconf.q
+++ test/sysconf.q
@@ -3,30 +3,26 @@
 %requires sysconf
 
 
-printf("%N\n", sysconf_constants());
+sub test(hash $c, string $function) {
+    my HashIterator $it($c);
+    my any $ret;
+    while ($it.next()) {
+        try {
+            if (exists $argv)
+                $ret = call_function_args($function, $argv + $it.getValue());
+            else
+                $ret = call_function($function, $it.getValue());
+            printf("%s: %s = %n\n", $function, $it.getKey(), $ret);
+        }
+        catch ($ex) {
+            printf("ERROR: %s: %s = %s:%s\n", $function, $it.getKey(), $ex.err, $ex.desc);
+        }
+    }
+}
+
+test(sysconf_constants(), "sysconf");
+test(confstr_constants(), "confstr");
+test(pathconf_constants(), "pathconf", "/etc/passwd");
 
 printf("\nCount of CPUs: %N\n\n", sysconf(Sysconf::SC_NPROCESSORS_ONLN));
-
-my hash $sc = sysconf_constants();
-
-foreach my string $i in (keys $sc) {
-    try {
-        my int $val = sysconf($sc.$i);
-        printf("%s -> %n\n", $i, $val == Sysconf::SC_NO_LIMIT ? "is not set (-1)" : $val);
-    } catch ($ex) {
-        printf("%s -> %s:%s\n", $i, $ex.err, $ex.desc);
-    }
-}
-
-
-my hash $cs = confstr_constants();
-
-printf("%N\n", $cs);
-foreach my string $i in (keys $cs) {
-    try {
-        my string $val = confstr($cs.$i);
-        printf("%s -> %n\n", $i, $val);
-    } catch ($ex) {
-        printf("%s -> %s:%s\n", $i, $ex.err, $ex.desc);
-    }
-}
+printf("%N\n", sysconf_constants());
